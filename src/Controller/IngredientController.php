9d362e4a5c6199f4bf9ced8ef752c0ce80277362
Quentin Cliquot <quentincliquot@gmail.com>
2022-10-27 01:59:50 +0200

    ✨ Adding CRUD for ingredient

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Ingredient;
 use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,9 +17,8 @@ class IngredientController extends AbstractController
 {
 
     /**
-     * This function display all ingredient
+     * This controller display all ingredient
      * 
-     * Undocument function
      * 
      * @param IngredientRepository $repository
      * @param PaginatorInterface $paginator
@@ -26,7 +26,7 @@ class IngredientController extends AbstractController
      * @return Response 
      */
 
-    #[Route('/ingredient', name: 'app_ingredient')]
+    #[Route('/ingredient', name: 'ingredient.index', methods: ['GET'])]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
 
@@ -40,6 +40,18 @@ class IngredientController extends AbstractController
             'ingredients' => $ingredients
         ]);
     }
+
+
+    /**
+     * This controller show a form which create an ingredient
+     *
+     * 
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     * 
+     */
+
     #[Route('ingredient/new', "ingredient.new", methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $manager): Response
     {
@@ -57,10 +69,81 @@ class IngredientController extends AbstractController
                 'success',
                 'Votre ingrédient a été créer avec succès !'
             );
+
+            return $this->redirectToRoute('ingredient.index');
         }
 
         return $this->render('pages/ingredient/new.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+/**
+ * This controller allows to modfy an ingredient
+ * 
+ * 
+ * @param Ingredient $ingredient
+ * @param Request $request
+ * @param EntityManagerInterface $manager
+ * 
+ */
+
+    #[Route('/ingredient/edit/{id}', 'ingredient.edit', methods: ['GET', 'POST'])]
+    public function edit(Ingredient $ingredient, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre ingrédient a été modifié avec succès !'
+            );
+
+            return $this->redirectToRoute('ingredient.index');
+        }
+
+        return $this->render('pages/ingredient/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * This controller allows to delete an ingredient
+     * 
+     * 
+     * @param EntityManagerInterface $manager
+     * @param Ingredient $ingredient
+     * 
+     */
+
+    #[Route('/ingredient/delete/{id}', 'ingredient.delete', methods: ['GET'])]
+    public function delete(EntityManagerInterface $manager, Ingredient $ingredient): Response
+    {
+
+        if (!$ingredient) {
+            $this->addFlash(
+                'warning',
+                'L\'ingrédient rechercher n\'a pas été trouvé !'
+            );
+
+            return $this->redirectToRoute('ingredient.index');
+        } else {
+            $manager->remove($ingredient);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre ingrédient a été supprimer avec succès !'
+            );
+
+            return $this->redirectToRoute('ingredient.index');
+        }
     }
 }
