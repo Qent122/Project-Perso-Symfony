8a9d93acee3d3d4a56aa0e947f94c1f48a1a1ae8
Quentin Cliquot <quentincliquot@gmail.com>
2022-11-11 07:32:19 +0100

    Fix bad code for hashing password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -23,8 +23,12 @@ class UserController extends AbstractController
      * @return Response
      */
     #[Route('/utilisateur/edition/{id}', name: 'user.edit', methods: ['GET', 'POST'])]
-    public function edit(User $user, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
-    {
+    public function edit(
+        User $user,
+        Request $request,
+        EntityManagerInterface $manager,
+        UserPasswordHasherInterface $hasher
+    ): Response {
 
         if (!$this->getUser()) {
             return $this->redirectToRoute('security.login');
@@ -67,9 +71,33 @@ class UserController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+
+    /**
+     * This controller allow us to edit user's password
+     *
+     * @param User $user
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordHasherInterface $hasher
+     * @return Response
+     */
     #[Route('/utilisateur/edition-mot-de-passe/{id}', 'user.edit.password', methods: ['GET', 'POST'])]
-    public function editPassword(User $user, Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $manager): Response
-    {
+    public function editPassword(
+        User $user,
+        Request $request,
+        EntityManagerInterface $manager,
+        UserPasswordHasherInterface $hasher
+    ): Response {
+
+
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('security.login');
+        }
+        if ($this->getUser() !== $user) {
+            return $this->redirectToRoute('recipe.index');
+        }
+
         $form = $this->createForm(UserPasswordType::class);
 
         $form->handleRequest($request);
@@ -78,10 +106,10 @@ class UserController extends AbstractController
                 $user,
                 $form->getData()['plainPassword']
             )) {
-                $user->setPassword($hasher->hashPassword(
-                    $user,
+                $user->setUpdatedAt(new \DateTimeImmutable());
+                $user->setPlainPassword(
                     $form->getData()['newPassword']
-                ));
+                );
 
                 $manager->persist($user);
                 $manager->flush();
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -52,12 +52,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[Assert\NotNull()]
     private ?\DateTimeImmutable $createdAt = null;
 
+    #[ORM\Column]
+    #[Assert\NotNull()]
+    private ?\DateTimeImmutable $updatedAt = null;
 
 
 
     public function __construct()
     {
         $this->createdAt = new \DateTimeImmutable;
+        $this->updatedAt = new \DateTimeImmutable;
+
     }
 
     public function getId(): ?int
@@ -176,6 +181,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setCreatedAt(\DateTimeImmutable $createdAt): self
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
